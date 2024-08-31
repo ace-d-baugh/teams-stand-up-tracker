4e6c4fdf0b4a78a93b2b50bf27e90f3cb7278e57
Ace Baugh <ace.d.baugh@gmail.com>
2024-08-31 09:17:52 -0400

    New production ready model for portfolio

---
+++ docs/team.ts
@@ -0,0 +1,98 @@
+export const Team = [
+  {
+    firstName: 'Jenson',
+    lastName: 'Wells',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Paisley',
+    lastName: 'Manning',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Kaius',
+    lastName: 'Rocha',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Sage',
+    lastName: 'Fisher',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Rowan',
+    lastName: 'Olsen',
+    hasGone: false,
+    showPhoto: false,
+  },
+  {
+    firstName: 'Willow',
+    lastName: 'Pierce',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Beckett',
+    lastName: 'Crawford',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Caspian',
+    lastName: 'Reed',
+    hasGone: false,
+    showPhoto: false,
+  },
+  {
+    firstName: 'Orion',
+    lastName: 'Patel',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Caelum',
+    lastName: 'Hall',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Lylah',
+    lastName: 'Davenport',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Gideon',
+    lastName: 'Barnes',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Magnus',
+    lastName: 'Harrison',
+    hasGone: false,
+    showPhoto: false,
+  },
+  {
+    firstName: 'Zayden',
+    lastName: 'Gomez',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Thane',
+    lastName: 'Singh',
+    hasGone: false,
+    showPhoto: true,
+  },
+  {
+    firstName: 'Kieran',
+    lastName: 'Wallace',
+    hasGone: false,
+    showPhoto: true,
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import {
   CdkDrag,
   CdkDropList,
   moveItemInArray,
+  transferArrayItem,
 } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { Team } from './assets/team'
+import { Team } from '../public/team';
 
 interface TeamMember {
   firstName: string;
   lastName: string;
-  photo: string;
   hasGone: boolean;
   showPhoto: boolean;
 }
@@ -33,94 +33,199 @@ interface TeamMember {
           Toggle Dark Mode
         </button>
       </div>
-      <div
-        class="team-tracker-container"
-        cdkDropList
-        cdkDropListOrientation="mixed"
-        (cdkDropListDropped)="drop($event)"
-      >
-        <div class="row row-cols-4 g-2">
-          @for (member of team; track member) {
-          <div class="team-member" (click)="select(member)" cdkDrag>
-            <div class="card h-100">
-              <div
-                *ngIf="member.hasGone"
-                class="check-mark position-absolute top-0 end-0"
-              >
-                <i
-                  class="bi bi-check"
-                  style="font-size: 30px; font-weight: 900;"
-                ></i>
-              </div>
+      <div class="move-lists">
+        <button class="button mx-2 mb-3" (click)="moveToActive()">
+          Move to Stand-Up
+        </button>
+        <button class="button mx-2 mb-3" (click)="clearStandUp()">
+          Clear Stand-Up
+        </button>
+      </div>
 
-              <!-- ******* START INITIAL VERSION ******* -->
-
-              <div
-                *ngIf="!member.showPhoto"
-                class="card-img-top"
-                [class.selected]="member.hasGone"
-              >
-                <div class="member-initials">
-                  {{ member.firstName[0] }}{{ member.lastName[0] }}
+      <div class="team-tracker-container">
+        <div
+          class="team-pool-list-container"
+          cdkDropList
+          id="team-pool-list-container"
+          cdkDropListSortingDisabled
+          [cdkDropListData]="team"
+          (cdkDropListDropped)="drop($event)"
+          [cdkDropListConnectedTo]="['active-team-tracker-container']"
+        >
+          <h5>The Team</h5>
+          <div class="col g-2">
+            @for (member of team; track member) {
+            <div class="team-member d-flex justify-content-center" cdkDrag>
+              <div class="card w-100">
+                <div
+                  class="card-body list member-name w-100"
+                  [class.selected]="member.hasGone"
+                >
+                  <h5
+                    class="card-title d-flex flex-column justify-content-center"
+                  >
+                    {{ member.firstName
+                    }}<span class="d-none d-md-block"
+                      ><br />{{ member.lastName }}</span
+                    >
+                  </h5>
                 </div>
               </div>
+            </div>
+            }
+          </div>
+        </div>
+        <div
+          class="active-team-tracker-container"
+          id="active-team-tracker-container"
+          cdkDropList
+          cdkDropListOrientation="mixed"
+          [cdkDropListData]="active"
+          (cdkDropListDropped)="drop($event)"
+          [cdkDropListConnectedTo]="['team-pool-list-container']"
+        >
+          <h5>In The Stand-Up: {{ active.length }}</h5>
+          <div
+            class="stand-up-container row g-2 justify-content-center"
+            [ngClass]="active.length > 12 ? 'row-cols-4' : 'row-cols-3'"
+          >
+            @for (member of active; track member) {
+            <div class="col" (click)="select(member)" cdkDrag>
+              <div class="card h-100">
+                <div
+                  *ngIf="member.hasGone"
+                  class="check-mark position-absolute top-0 end-0"
+                >
+                  <i
+                    class="bi bi-check"
+                    style="font-size: 30px; font-weight: 900;"
+                  ></i>
+                </div>
 
-              <!-- ******* END INITIAL VERSION ******* -->
+                <!-- ******* START INITIAL VERSION ******* -->
 
-              <!-- ******* START PHOTO VERSION ******* -->
+                <div
+                  *ngIf="!member.showPhoto || !isValidPhoto(member.firstName)"
+                  class="card-img-top member-initials"
+                  [class.selected]="member.hasGone"
+                >
+                  <div class="initials">
+                    {{ member.firstName[0] }}{{ member.lastName[0] }}
+                  </div>
+                </div>
 
-              <div
-                *ngIf="member.showPhoto"
-                class="card-img-top member-photo"
-                [class.selected]="member.hasGone"
-              >
-                <img
-                  src="https://github.com/ace-d-baugh/teams-stand-up-tracker/blob/master/src/assets/{{
-                    member.photo
-                  }}?raw=true/"
-                />
-              </div>
+                <!-- ******* END INITIAL VERSION ******* -->
 
-              <!-- ******* END PHOTO VERSION ******* -->
+                <!-- ******* START PHOTO VERSION ******* -->
 
-              <div
-                class="card-body member-name"
-                [class.selected]="member.hasGone"
-              >
-                <h5 class="card-title h-100 text-center">
-                  {{ member.firstName }}
-                  <span class="d-none d-md-block">{{ member.lastName }}</span>
-                </h5>
+                <div
+                  *ngIf="member.showPhoto && isValidPhoto(member.firstName)"
+                  class="card-img-top member-photo"
+                  [class.selected]="member.hasGone"
+                >
+                  <img
+                    src="{{ member.firstName }}.jpg"
+                    alt="{{ member.firstName }} {{ member.lastName }}"
+                  />
+                </div>
+
+                <!-- ******* END PHOTO VERSION ******* -->
+
+                <div
+                  class="card-body member-name"
+                  [class.selected]="member.hasGone"
+                >
+                  <h5 class="card-title h-100 text-center">
+                    {{ member.firstName }}
+                    <span class="d-none d-md-block"
+                      ><br />{{ member.lastName }}</span
+                    >
+                  </h5>
+                </div>
               </div>
             </div>
+            }
           </div>
-          }
         </div>
       </div>
     </div>
   `,
 })
 export class App implements OnInit {
+  isValidPhoto(firstName: string): boolean {
+    return this.photoExists(`${firstName}.jpg`);
+  }
+
+  photoExists(url: string): boolean {
+    const xhr = new XMLHttpRequest();
+    xhr.open('HEAD', url, false);
+    xhr.send();
+    return xhr.status !== 404;
+  }
+
+  clearStandUp() {
+    this.team.push(...this.active);
+    this.active = [];
+    // Sort by first name
+    this.team.sort((a, b) => (a.firstName > b.firstName ? 1 : -1));
+  }
+  moveToActive() {
+    this.active.push(...this.team);
+    this.team = [];
+  }
   team: TeamMember[] = [];
+  active: TeamMember[] = [];
   isDarkMode = false;
 
   ngOnInit(): void {
     this.team = Team;
+    this.team.sort((a, b) => (a.firstName > b.firstName ? 1 : -1));
+    this.loadThemePreference();
   }
 
   select(member: TeamMember): void {
     member.hasGone = !member.hasGone;
   }
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.team, event.previousIndex, event.currentIndex);
+  drop(event: CdkDragDrop<TeamMember[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+    console.log('active', this.active.length);
   }
 
   toggleDarkMode(): void {
     this.isDarkMode = !this.isDarkMode;
+    this.saveThemePreference();
     if (this.isDarkMode) {
       document.body.classList.add('dark-mode');
     } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
+
+  private saveThemePreference(): void {
+    localStorage.setItem('themePreference', this.isDarkMode ? 'dark' : 'light');
+  }
+
+  private loadThemePreference(): void {
+    const themePreference = localStorage.getItem('themePreference');
+    if (themePreference === 'dark') {
+      this.isDarkMode = true;
+      document.body.classList.add('dark-mode');
+    } else {
+      this.isDarkMode = false;
       document.body.classList.remove('dark-mode');
     }
   }
